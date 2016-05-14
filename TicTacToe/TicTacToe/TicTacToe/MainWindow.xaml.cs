30efb796eeab4b95a4de116b7c1cf74fb85dad47
Keshav Gupta <keshav1032@gmail.com>
2016-05-14 22:01:18 +0530

    Added game algo

--- TicTacToe/TicTacToe/TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/TicTacToe/TicTacToe/MainWindow.xaml.cs
@@ -23,11 +23,14 @@ namespace TicTacToe
 
         public string User0 { get; set; }
         public string User1 { get; set; }
+        public LinkedList<int> MarkedUser0;
+        public LinkedList<int> MarkedUser1;
 
         public int Turn { get; set; }
         public MainWindow()
         {
-
+            MarkedUser0 = new LinkedList<int>();
+            MarkedUser1 = new LinkedList<int>();
             Turn = 0;            
             InitializeComponent();
         }
@@ -37,14 +40,17 @@ namespace TicTacToe
             if(Turn == 0)
             {
                 button1.Content = "X";
+                MarkedUser0.AddFirst(1);
             }
             else
             {
                 button1.Content = "O";
+                MarkedUser1.AddFirst(1);
             }
 
-
+            MarkedUser0.AddFirst(1);
             Turn  = ( Turn + 1 ) % 2;
+            CheckWin();
 
         }
 
@@ -53,14 +59,19 @@ namespace TicTacToe
             if (Turn == 0)
             {
                 button2.Content = "X";
+                MarkedUser0.AddFirst(2);
+
             }
             else
             {
                 button2.Content = "O";
+                MarkedUser1.AddFirst(2);
+
             }
 
 
             Turn  = ( Turn + 1 )% 2;
+            CheckWin();
 
         }
 
@@ -69,14 +80,19 @@ namespace TicTacToe
             if (Turn == 0)
             {
                 button3.Content = "X";
+                MarkedUser0.AddFirst(3);
+
             }
             else
             {
                 button3.Content = "O";
+                MarkedUser1.AddFirst(3);
+
             }
 
 
             Turn  = ( Turn + 1 )% 2;
+            CheckWin();
 
         }
 
@@ -85,14 +101,19 @@ namespace TicTacToe
             if (Turn == 0)
             {
                 button4.Content = "X";
+                MarkedUser0.AddFirst(4);
+
             }
             else
             {
                 button4.Content = "O";
+                MarkedUser1.AddFirst(4);
+
             }
 
 
             Turn  = ( Turn + 1 )% 2;
+            CheckWin();
 
         }
 
@@ -101,14 +122,19 @@ namespace TicTacToe
             if (Turn == 0)
             {
                 button5.Content = "X";
+                MarkedUser0.AddFirst(5);
+
             }
             else
             {
                 button5.Content = "O";
+                MarkedUser1.AddFirst(5);
+
             }
 
 
             Turn  = ( Turn + 1 )% 2;
+            CheckWin();
 
         }
 
@@ -117,14 +143,19 @@ namespace TicTacToe
             if (Turn == 0)
             {
                 button6.Content = "X";
+                MarkedUser0.AddFirst(6);
+
             }
             else
             {
                 button6.Content = "O";
+                MarkedUser1.AddFirst(6);
+
             }
 
 
             Turn  = ( Turn + 1 )% 2;
+            CheckWin();
 
         }
 
@@ -133,14 +164,19 @@ namespace TicTacToe
             if (Turn == 0)
             {
                 button7.Content = "X";
+                MarkedUser0.AddFirst(7);
+
             }
             else
             {
                 button7.Content = "O";
+                MarkedUser1.AddFirst(7);
+
             }
 
 
             Turn  = ( Turn + 1 )% 2;
+            CheckWin();
 
         }
 
@@ -149,14 +185,19 @@ namespace TicTacToe
             if (Turn == 0)
             {
                 button8.Content = "X";
+                MarkedUser0.AddFirst(8);
+
             }
             else
             {
                 button8.Content = "O";
+                MarkedUser1.AddFirst(8);
+
             }
 
 
             Turn  = ( Turn + 1 )% 2;
+            CheckWin();
 
         }
 
@@ -165,15 +206,73 @@ namespace TicTacToe
             if (Turn == 0)
             {
                 button9.Content = "X";
+                MarkedUser0.AddFirst(9);
+
             }
             else
             {
                 button9.Content = "O";
+                MarkedUser1.AddFirst(9);
             }
 
 
             Turn  = ( Turn + 1 )% 2;
+            CheckWin();
+        }
 
+        private void Reset()
+        {
+            MarkedUser0 = new LinkedList<int>();
+            MarkedUser1 = new LinkedList<int>();
+            button1.Content = "";
+            button2.Content = "";
+            button3.Content = "";
+            button4.Content = "";
+            button5.Content = "";
+            button6.Content = "";
+            button7.Content = "";
+            button8.Content = "";
+            button9.Content = "";
+            Turn = 0;
+            label.Content = "";
+        }
+
+        private void CheckWin()
+        {
+            if((MarkedUser0.Contains(1) && MarkedUser0.Contains(5) && MarkedUser0.Contains(9)) ||
+               (MarkedUser0.Contains(3) && MarkedUser0.Contains(5) && MarkedUser0.Contains(7)) ||
+
+               (MarkedUser0.Contains(1) && MarkedUser0.Contains(2) && MarkedUser0.Contains(3)) ||
+               (MarkedUser0.Contains(4) && MarkedUser0.Contains(5) && MarkedUser0.Contains(6)) ||
+               (MarkedUser0.Contains(7) && MarkedUser0.Contains(8) && MarkedUser0.Contains(9)) ||
+
+               (MarkedUser0.Contains(1) && MarkedUser0.Contains(4) && MarkedUser0.Contains(7)) ||
+               (MarkedUser0.Contains(2) && MarkedUser0.Contains(5) && MarkedUser0.Contains(8)) ||
+               (MarkedUser0.Contains(3) && MarkedUser0.Contains(6) && MarkedUser0.Contains(9)))
+            {
+                this.Title = "Winner is User 1 (X)";
+                this.label.Content = "Winner is User 1 (X)";
+            }
+
+            if ((MarkedUser1.Contains(1) && MarkedUser1.Contains(5) && MarkedUser1.Contains(9)) ||
+               (MarkedUser1.Contains(3) && MarkedUser1.Contains(5) && MarkedUser1.Contains(7)) ||
+
+               (MarkedUser1.Contains(1) && MarkedUser1.Contains(2) && MarkedUser1.Contains(3)) ||
+               (MarkedUser1.Contains(4) && MarkedUser1.Contains(5) && MarkedUser1.Contains(6)) ||
+               (MarkedUser1.Contains(7) && MarkedUser1.Contains(8) && MarkedUser1.Contains(9)) ||
+
+               (MarkedUser1.Contains(1) && MarkedUser1.Contains(4) && MarkedUser1.Contains(7)) ||
+               (MarkedUser1.Contains(2) && MarkedUser1.Contains(5) && MarkedUser1.Contains(8)) ||
+               (MarkedUser1.Contains(3) && MarkedUser1.Contains(6) && MarkedUser1.Contains(9)))
+            {
+                this.Title = "Winner is User 2 (O)";
+                this.label.Content = "Winner is User 2 (O)";
+            }
+        }
+
+        private void button_reset_Click(object sender, RoutedEventArgs e)
+        {
+            Reset();
         }
     }
 
